9f8e952f42ff92100602bd6e96b31f4fcde98e82
andrijard <radivojevandrija007@gmail.com>
2023-04-08 07:03:54 -0400

    bucket

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,52 +1,45 @@
 import React from "react";
 import { Navbar } from "../Navbar/Navbar";
 import './style.css'
-//import { firstCapittal } from '../Global/Global';
+
+const link = "https://nexus-online-school-database.s3.eu-central-1.amazonaws.com/Data/Home.json"
 
 
 function SubjectBubble({ image, color, title }){
     var imageStyle = {
         'backroundColor': color
     }
-
-    const [imagePath, setImagePath] = React.useState(null);
-
-    React.useEffect(() => {
-        import(`../../assets/SubjectIcons/${image}`)
-        .then((image) => setImagePath(image))
-        .catch((error) => console.log(error));
-    }, [image]);
     
     return (
         <div className="subjectBubble" id={title}>
-            <img src={imagePath} alt="" style={imageStyle}/>
+            <img src={image} alt="" style={imageStyle}/>
             <h1>{title}</h1>
         </div>
     );
 }
 
-//Home.json
-var HomeJSON = [
-    {
-        "SubjectName": "Anatomija",
-        "SubjectColor": "rgb(203, 48, 125)",
-        "SubjectImage": "anatomija.png"
-    }
-]
-
-const SubjectImagePath = "../../assets/SubjectIcons/"
 
 export function Home(){
     document.title = "Nexus"
+    const [data, setData] = React.useState([]);
 
+    React.useEffect(() => {
+            const fetchData = async () => {
+            const response = await fetch(link);
+            const jsonData = await response.json();
+            setData(jsonData);
+        };
+
+        fetchData();
+    }, []);
 
     return(
         <div className="body">
             <Navbar />
-            {HomeJSON.map((data, index) => {
+            {data.map((data, index) => {
                 console.log(data)
                 return(
-                    <SubjectBubble key={index} image={SubjectImagePath + data.SubjectImage} color={data.SubjectColor} title={data.SubjectName} />
+                    <SubjectBubble key={index} image={data.SubjectImage} color={data.SubjectColor} title={data.SubjectName} />
                 )
             })}
         </div>
